c13e8e997e9353a2c21da7056a9996ba5b849475
Goolzzi <goolzzijeon@gmail.com>
2017-11-22 19:57:00 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import printMe from './print.js'
+import './style.css';
+
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'MonoRepo'], ' ');
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
